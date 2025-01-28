134b859164f4d8dd4b44a86b9f90272b51447aec
fakhir hassan <fakhirhassan@fakhirs-MacBook-Pro.local>
2025-01-29 02:08:02 +0500

    remaining files

--- logger/app.py
+++ logger/app.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
-        logging.FileHandler("app.log"),
+        logging.FileHandler("app1.log"),
         logging.StreamHandler()
     ]
 )
